e9f621ec6803fb4e112132cea1411983159898b0
Khayrullin Oleg <49640766+techmag112@users.noreply.github.com>
2023-01-21 10:02:09 +0300

    Add files via upload

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from '../header/header';
 import HeaderExt from '../header-ext/headerExt';
@@ -10,31 +10,24 @@ import DBService from '../services/DBService';
 
 import './style.css'; 
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pageNow: '',
-            term : '',
-            filter: ''
-        }
-        this.dbcoffee = []
-        this.dbtop = []
-        this.currentCountry = ''
-        this.currentPrice = ''
-    }
+const App = () => {
+    const [pageNow, setPageNow] = useState(1);
+    const [term, setTerm] = useState('');
+    const [filter, setFilter] = useState('');
+    const [dbtop, setDBtop] = useState([]);
+    const [dbcoffee, setDBcoffee] = useState([]);
+    const [currentCountry, setCurrentCountry] = useState('');
+    const [currentPrice, setCurrentPrice] = useState('');
+
+    // Загружаем базы из внешнего источника при первом формировании DOM
+    useEffect(() => {
+        const dbservice = new DBService()
+        setDBtop(dbservice.getBestKind());
+        setDBcoffee(dbservice.getDBCofee());
+    }, []);
     
-    dbservice = new DBService()
-
-    componentDidMount() {
-        // Загружаем базы при формировании DOM
-        this.dbtop = this.dbservice.getBestKind()
-        // Прописываем номер страницы по умолчанию и делаем ререндинг компонент
-        this.setState({ pageNow: 1 })
-        this.dbcoffee = this.dbservice.getDBCofee()
-    } 
-
-    addFilter = (items, term, filter) => {
+    // Добавление на базу кофе фильтров
+    const addFilter = (items, term, filter) => {
         let arr = [];
         if ((filter.length === 0) && (term.length === 0)) {
                 return items;
@@ -51,46 +44,45 @@ class App extends Component {
     }
 
     // Переключение черех меню между страницами
-    onUpdatePage = (page) => {
+    const onUpdatePage = (page) => {
         switch (page) { 
-            case '2': this.setState({ pageNow: 2 });
+            case '2': setPageNow(2);
                       break;  
-            case '3': this.setState({ pageNow: 3 });
+            case '3': setPageNow(3);
                       break;                
-            case '4': this.setState({ pageNow: 4 });
+            case '4': setPageNow(4);
                       break;  
-            default: this.setState({ pageNow: 1 });
+            default: setPageNow(1);
         }
     }
    
     // Обновить текст поиска
-    onUpdateSearch = (term) => {
-        this.setState({term});
+    const onUpdateSearch = (term) => {
+        setTerm(term);
     }
 
     // Обновить фильтр по кнопкам
-    onFilterSelect = (filter) => {
-        if (this.state.filter === filter) {
-            this.setState({
-                filter: '' });    
+    const onFilterSelect = (newFilter) => {
+        if (filter === newFilter) {
+            setFilter('');    
         } else {
-            this.setState({filter});
+            setFilter(newFilter);    
         }
     }
 
-    onCardSelected = (country, price) => {
-        this.currentCountry = country
-        this.currentPrice = price
-        this.setState({ pageNow: 4 })
+    const onCardSelected = (country, price) => {
+        setCurrentCountry(country);
+        setCurrentPrice(price);
+        setPageNow(4);
     }
 
     // Рендер карточек магазина
-    renderShopCards = (arr) => {
+    const renderShopCards = (arr) => {
         let cards = arr.map((item,index) => {
           const nameCoffee = `${item.kind} ${item.weight} kg`
           const img = `./img/${item.urlimg}`
           return (
-                  <div className="shop_card" key={index} onClick = {() => this.onCardSelected(item.country, item.price)}>
+                  <div className="shop_card" key={index} onClick = {() => onCardSelected(item.country, item.price)}>
                       <img src={img} alt={item.kind} className="coffee_pic"/>
                       <div className="coffee_name">{nameCoffee}</div>
                       <div className="coffee_country">{item.country}</div>
@@ -101,7 +93,7 @@ class App extends Component {
     }
 
     // Рендер топ 3 кофе
-    renderTopCards = (arr) => { 
+    const renderTopCards = (arr) => { 
         let topCards = arr.map((item,index) => {
         const imgURL = `./img/${item.img}`
         const altBestTop = `Best Coffee ${index}`
@@ -116,7 +108,7 @@ class App extends Component {
     }
 
     // Рендер вехнее или нижнее меню
-    renderMenu = (color='black') => {
+    const renderMenu = (color='black') => {
         color = color !== 'black' ? {color: 'white'} : {color: 'black', marginTop: '22px'}
         const buttonsData = [
             {name: 'Coffee house'},
@@ -124,13 +116,14 @@ class App extends Component {
             {name: 'For your pleasure'},
         ];
         const buttons = buttonsData.map((item,index) => {
+            // Синхронизируем отображение выбора пунктов двух меню через доп класс css
+            const activeButton = pageNow === index+1 ? 'menu__link active' : 'menu__link'
             return (
                 <li className="header__item" key={index}>
                     <button type="button" 
-                                 className='menu__link'
-                                 disabled = {this.pageNow ? true : false}
+                                 className = {activeButton}
                                  style={color}
-                                 onClick={() => this.onUpdatePage(`${index+1}`)}>
+                                 onClick={() => onUpdatePage(`${index+1}`)}>
                                     {item.name}
                                  </button>
                 </li>
@@ -140,15 +133,13 @@ class App extends Component {
         return buttons
     }
     
-    render() {
-       const {pageNow, term, filter} = this.state;  
-       // Оставить только видимые элементы базы кофе
-       const visibleData = this.addFilter(this.dbcoffee, term, filter); 
-       let img = '';
-       let title = '';
-       let headerExt = null;
-       let headerTitle = null;
-       let styleFooter = null;
+    // Оставить только видимые элементы базы кофе
+    const visibleData = addFilter(dbcoffee, term, filter); 
+    let img = '';
+    let title = '';
+    let headerExt = null;
+    let headerTitle = null;
+    let styleFooter = null;
        switch (pageNow) {
             case 2: 
                 img = 'coffee-shop.jpg';
@@ -170,48 +161,48 @@ class App extends Component {
                 break;
             default:
                 img = 'Main_bg.jpg'
-                headerExt = <HeaderExt  onUpdatePage={this.onUpdatePage}/>
+                headerExt = <HeaderExt  onUpdatePage={onUpdatePage}/>
         }
         const headerUrl = {
                  background: `url(../img/${img})`
              }
          // backgroundRepeat: 'no-repeat'    
         
-        return (
+    return (
             <div className="container">
                 <header>
                      <div className="wrapper" style={headerUrl}>
-                         <Header  renderMenu={this.renderMenu}/>
+                         <Header  renderMenu={renderMenu}/>
                          {headerTitle}
                          {headerExt} 
                      </div>
                 </header>
                 <main>
-                    <Section1 pageNow={pageNow} country={this.currentCountry} price={this.currentPrice} />
+                    <Section1 pageNow={pageNow} country={currentCountry} price={currentPrice} />
                     <div className="coffee_shop">
                         <FilterPanel 
                             pageNow = {pageNow} 
                             filter = {filter} 
-                            onFilterSelect={this.onFilterSelect} 
-                            onUpdateSearch={this.onUpdateSearch}
+                            onFilterSelect={onFilterSelect} 
+                            onUpdateSearch={onUpdateSearch}
                         />
                         <Section2 
                             pageNow={pageNow} 
                             arr={visibleData} 
-                            dbtop={this.dbtop} 
-                            renderTopCards={this.renderTopCards} 
-                            renderShopCards={this.renderShopCards} 
+                            dbtop={dbtop} 
+                            renderTopCards={renderTopCards} 
+                            renderShopCards={renderShopCards} 
                         />
                     </div>
                  </main>
                  <footer>
                     <div className="wrapper__footer" style={styleFooter}>
-                        <Footer renderMenu={this.renderMenu}/>
+                        <Footer renderMenu={renderMenu}/>
                     </div>
                 </footer>
             </div>
-        )  
-    } 
+        )   
 }
-		
+
+
 export default App;
